1f36e4a6429f900505aa09edaa7a070e31e9cf34
Joseph Umana <joseph@josephumana.com>
2026-09-03 16:51:37 -0400

    formatting
    Signed-off-by: Joseph Umana <joseph@josephumana.com>

--- src/app.cpp
+++ src/app.cpp
@@ -30,23 +30,32 @@ void RestrApp::switchCollection(int& index, const ftxui::MenuOption& option, std
     entries.clear();
     getDocumentNames(index, entries);
 }
-
 void RestrApp::run() {
-    auto screen { ftxui::App::Fullscreen() };
+    auto screen {
+        ftxui::App::Fullscreen()
+    };
 
-    std::vector<std::string> entries { getCollectionNames() };
+    std::vector < std::string > entries {
+        getCollectionNames()
+    };
 
-    int selected { 0 };
+    int selected {
+        0
+    };
 
-    std::vector<std::string> documentEntries;
+    std::vector < std::string > documentEntries;
 
     getDocumentNames(selected, documentEntries);
 
-    int documentSelected { 0 };
+    int documentSelected {
+        0
+    };
 
     ftxui::MenuOption menuOption;
     menuOption.on_enter = screen.ExitLoopClosure();
-    menuOption.on_change = [&] () { switchCollection(selected, menuOption, documentEntries); };
+    menuOption.on_change = [ & ]() {
+        switchCollection(selected, menuOption, documentEntries);
+    };
     auto menu = ftxui::Menu(&entries, &selected, menuOption);
 
     ftxui::MenuOption documentOption;
@@ -54,18 +63,28 @@ void RestrApp::run() {
 
     auto documentMenu = ftxui::Menu(&documentEntries, &documentSelected, documentOption);
 
-    auto mainComponent = ftxui::Container::Horizontal({menu, documentMenu});
+    auto mainComponent = ftxui::Container::Horizontal({
+        menu,
+        documentMenu
+    });
 
-    auto uiRenderer = ftxui::Renderer(mainComponent, [&] {
-        auto menuWin = menu->Render();
-        auto docMenuWin = documentMenu->Render();
+    auto uiRenderer = ftxui::Renderer(mainComponent, [ & ] {
+        auto menuWin = menu -> Render();
+        auto docMenuWin = documentMenu -> Render();
 
         return ftxui::vbox({
-                ftxui::hbox({ftxui::vbox({ ftxui::text("API Collections"), menuWin | ftxui::border | ftxui::flex}), ftxui::vbox({ftxui::text("API Collections"), docMenuWin | ftxui::border | ftxui::flex}) | ftxui::flex }) | ftxui::flex,
+            ftxui::hbox({
+                ftxui::vbox({
+                    ftxui::text("API Collections"),
+                    menuWin | ftxui::border | ftxui::flex
+                }),
+                ftxui::vbox({
+                    ftxui::text("API Collections"),
+                    docMenuWin | ftxui::border | ftxui::flex
+                }) | ftxui::flex
+            }) | ftxui::flex,
         });
     });
 
-    
     screen.Loop(uiRenderer);
 }
-
